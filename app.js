c53e682ac5554475a3f6e9e30f8b6fc33a3ddba6
LuisCaro92 <luis.carosalgado1@gmail.com>
2022-12-02 19:24:49 +0000

    comentrioversionpropia

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+let comO = ["compra", "vende", "elige", "compara"]
+let desdE = ["entucasa", "aqui", "tumismo", "hoy", "desdeaca", "cuandoquieras", "loquesea"]
+let dominiO = [".com", ".net", ".cl", ".ca", ".io", ".pe", ".ar"]
+
+
+for (i = 0; i < comO.length; i++) 
+        for (j = 0; j < desdE.length; j++)
+                for (k = 0; k < dominiO.length; k++) {
+
+
+                        console.log(comO[i] + desdE[j] + dominiO[k])
+                }
+               
